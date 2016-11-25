f3d447425ab05a20a7418b2bab5883d61fc2487f
Taras Shkodenko <taras@shkodenko.com>
2016-11-25 20:27:08 +0200

    Updated form.php, index.html

--- form.php
+++ form.php
@@ -3,9 +3,11 @@
  * Created by PhpStorm.
  * User: 23
  * Date: 23.11.16
- * Time: 20:55
  */
 
+ 
+ 
 if (!empty($_POST)) {
-    echo '<p>Post data:</p><pre>' . var_export($_POST, 1) . '</pre>';
+    echo '<p>Sent post data:</p>' . 
+		'<pre>' . var_export($_POST, 1) . '</pre>';
 }
